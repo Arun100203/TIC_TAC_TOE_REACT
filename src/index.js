d8ae96cb67e77d5f709d2c2fdfe2d4ecf5ad58b8
ARUN L <108601293+Arun100203@users.noreply.github.com>
2023-09-05 10:19:43 +0530

    Update component name

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App";
+import Board from "./Board";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <Board />
   </StrictMode>
 );
